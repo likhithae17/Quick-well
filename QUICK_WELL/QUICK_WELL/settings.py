30250019be5d9cdc0c4d29f6659760106a74bda4
likhithae17 <likhitha.e17@iiits.in>
2018-11-18 04:05:18 +0530

    pulled

--- QUICK_WELL/QUICK_WELL/settings.py
+++ QUICK_WELL/QUICK_WELL/settings.py
@@ -246,4 +246,9 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'quickwelldoctor@gmail.com'
+EMAIL_HOST_PASSWORD = 'doctor@123'
--- QUICK_WELL/docapp/forms.py
+++ QUICK_WELL/docapp/forms.py
@@ -1,20 +1,22 @@
 import datetime
 
-from django.forms import ModelForm, DateInput, TimeInput, forms
+from django.forms import ModelForm, DateInput, TimeInput, TextInput, EmailInput, forms
 from .models import Appointment
 
 class AppointmentForm(ModelForm):
     class Meta:
         model = Appointment
-        fields = ['date','time', 'pathlab']
+        fields = ['date','time']
         widgets = {
             'date': DateInput(attrs={'type': 'date'}),
             'time': TimeInput(attrs={'type': 'time'}),
+            'username':TextInput(attrs={'type': 'text'}),
+            'emailid': EmailInput(attrs={'type':'email'}),
         }
 
     def clean_date(self):
         date = self.cleaned_data['date']
         if date < datetime.date.today():
-            raise forms.ValidationError("The date cannot be in the past!")
+            raise forms.ValidationError("Sorry, Invalid Date!")
         return date
 
