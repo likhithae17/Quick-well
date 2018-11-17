c168a1dd698395be57ef9e9390f74b37ee53b22a
Srishti365 <srishti.e17@iiits.in>
2018-11-17 22:25:51 +0530

    front end

--- QUICK_WELL/home/views.py
+++ QUICK_WELL/home/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
+
+
+
 def home(request):
     return render(request, 'home/home.html')
