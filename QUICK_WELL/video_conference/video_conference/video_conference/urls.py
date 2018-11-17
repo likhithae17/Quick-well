5760aa779fe2db153c7a5bdc9d81192a984d2588
jaswanthb123 <jaswanth.b17@iiits.in>
2018-11-17 15:36:24 +0530

    changed video_conference

---
+++ QUICK_WELL/video_conference/video_conference/video_conference/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import auth_login
+urlpatterns = [
+    path('', views.home, name='video-request'),
+    path('mail', views.Mail),
+    #path('login',login)
+]
