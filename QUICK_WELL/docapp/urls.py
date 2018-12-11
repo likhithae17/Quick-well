323c1265423d7fd379f45a5ec0dfab5e918e9526
sandeshjatla <venkatasivadurga.j17@iiits.in>
2018-12-12 02:44:11 +0530

    ui

--- QUICK_WELL/docapp/urls.py
+++ QUICK_WELL/docapp/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('greet/', views.greet, name='greet'),
     path('profile/<int:pk>', views.profile, name='profile'),
     path('delete/<int:pk>',views.delete, name='delete'),
-    path('profile/<int:pk>/home/', views.index1, name='index1'),
-    path('profile/<int:pk>/contact', views.contact, name='contact'),
-    path('profile/<int:pk>/test', views.test, name='test'),
+    # path('profile/<int:pk>/home/', views.index1, name='index1'),
+    # path('profile/<int:pk>/contact', views.contact, name='contact'),
+    # path('profile/<int:pk>/test', views.test, name='test'),
 
     #path('book/<int:pk>/', views.confirm, name='confirm'),
     # re_path('appbooking/?P<pk>[0-9]/',views.appbooking,name='appbooking'),
