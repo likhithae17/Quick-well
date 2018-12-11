94e024c7d60ef5fec96164aebdf667a9c01d1ddf
sandeshjatla <venkatasivadurga.j17@iiits.in>
2018-12-12 03:42:11 +0530

    uidone

--- QUICK_WELL/accounts/forms.py
+++ QUICK_WELL/accounts/forms.py
@@ -11,6 +11,13 @@ class Signup_user_form(forms.ModelForm):
         model = User
         fields = ['username', 'email', 'password']
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError(u'Email id already exists')
+        return email
+
 
 class Signup_profile_form(forms.ModelForm):
     class Meta:
